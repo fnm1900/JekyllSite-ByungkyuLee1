b36ab6feaf4f994aa87b42432dc7613c7a71630a
Byungkyu Lee <>
2021-05-07 06:27:18 +0000

    Stedman Session 2

---
+++ site/_includes/nav.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  var menu = document.querySelector(".menu-trigger");
+  menu.addEventListener("click", e => {
+    document.querySelector(".nav-links ul").classList.toggle("open");
+  });
+</script>
